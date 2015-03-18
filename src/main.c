7b37af39c90a342dce603f3c5a1ff39418f1edc3
Sequoya42 <rbaum@e1r5p22.42.fr>
2015-03-18 04:27:21 +0100

    minor changes

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: rbaum <rbaum@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/03/12 17:21:57 by rbaum             #+#    #+#             */
-/*   Updated: 2015/03/18 03:48:07 by rbaum            ###   ########.fr       */
+/*   Updated: 2015/03/18 04:25:31 by rbaum            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,11 +40,16 @@ int			main(int ac, char **av)
 	S->tab = ft_strdup_tab(av + 1);
 	S->lst = NULL;
 	S->lb = NULL;
+	S->oper = ft_strnew(1);
 	if (S->tab == NULL)
 		return (-1);
 	if ((verif_tab()) == -1)
 		return (-1);
 	add_link();
-	ft_testa();
+	print_list();
+	ft_resolve();
+	print_list();
+	write(1, S->oper, ft_strlen(S->oper) -1);
+	ft_putchar('\n');
 	return (0);
 }
--- src/resolve.c
+++ src/resolve.c
@@ -6,32 +6,52 @@
 /*   By: rbaum <rbaum@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/03/18 03:48:41 by rbaum             #+#    #+#             */
-/*   Updated: 2015/03/18 04:08:35 by rbaum            ###   ########.fr       */
+/*   Updated: 2015/03/18 04:26:35 by rbaum            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void		ft_get_min(void)
+void			ft_get_min(void)
 {
+	t_lst		*tmp;
+
+	tmp = S->lst;
 	S->min = S->lst->nb;
+	
 	while (S->lst)
 	{
 		if (S->min > S->lst->nb)
 			S->min = S->lst->nb;
 		S->lst = S->lst->next;
 	}
+	S->lst = tmp;
 }
 
-void		ft_check_swap();
-void		ft_resolve(void)
+void			ft_check_swap(void)
+{
+	t_lst		*tmp1;
+	t_lst		*tmp2;
+
+	tmp1 = S->lst->next->next;
+	tmp2 = S->lst->next->next->next;
+	if (S->lst->nb > S->lst->next->nb && tmp2 > tmp1)
+	{
+		ft_pb();
+		ft_pb();
+		ft_ss();
+		ft_pa();
+		ft_pa();
+		S->oper = ft_strjoin(S->oper, "pb pb ss pa pa ");
+	}
+	else if (S->lst->nb > S->lst->next->nb)
+	{
+		ft_sa();
+		S->oper = ft_strjoin(S->oper, "sa ");
+	}
+}
+void			ft_resolve(void)
 {
 	ft_get_min();
 	ft_check_swap();
-	if (S->lst->nb > S->lst->next->nb)
-	{
-		ft_sa();
-		oper = ft_strjoin(oper, "sa ");
-	}
-	
 }
